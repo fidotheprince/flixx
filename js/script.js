ac5449246f9e367c907083461aa0e013bf23d0c4
Alvaro Castro Cid <alvarocastrocid@Alvaros-MacBook-Pro.local>
2024-01-10 19:57:07 -0500

    added shows to shows.html;

--- js/script.js
+++ js/script.js
@@ -19,33 +19,67 @@ const fetchFromTMBD = async (endpoint) => {
 
 const date = timeStamp => new Date(timeStamp).toLocaleDateString("en-US", { year: 'numeric', month: 'long', day: 'numeric' });
 
+//Movie Card
+const movieCard = (title, releaseDate, src, id) =>`
+    <div class="card">
+        <a href="movie-details.html?id=${id}">
+        <img
+            src=${src}
+            class="card-img-top"
+            alt="Movie Title"
+        />
+        </a>
+        <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">
+            <small class="text-muted">Release: ${date(releaseDate)}</small>
+        </p>
+    </div>
+`;
+
+//Show Card
+const showCard = (name, timeStamp, src, id) => `
+    <div class="card">
+        <a href="tv-details.html?id=${id}">
+        <img
+            src=${src}
+            class="card-img-top"
+            alt="Show Title"
+        />
+        </a>
+        <div class="card-body">
+        <h5 class="card-title">${name}</h5>
+        <p class="card-text">
+            <small class="text-muted">Aired: ${date(timeStamp)}</small>
+        </p>
+        </div>
+    </div>
+`
+
 const renderMovies = async () => {
     const { results } = await fetchFromTMBD('movie/popular');
 
     results.forEach(movie => {
+        const src = movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : 'images/no-image.jpg';
         const parent = document.querySelector('#popular-movies');
         const child = document.createElement('div');
-        const src = movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : 'images/no-image.jpg';
-        
         child.classList.add('card');
-        
-        const cardBody = `
-            <div class="card">
-                <a href="movie-details.html?id=${movie.id}">
-                <img
-                    src=${src}
-                    class="card-img-top"
-                    alt="Movie Title"
-                />
-                </a>
-                <div class="card-body">
-                <h5 class="card-title">${movie.title}</h5>
-                <p class="card-text">
-                    <small class="text-muted">Release: ${date(movie.release_date)}</small>
-                </p>
-            </div>
-        `;
+        const cardBody = movieCard(movie.title, movie.release_date, src, movie.id);
+        child.innerHTML += cardBody;
+        parent.appendChild(child);
 
+    });
+}
+
+const renderShows = async () => {
+    const { results } = await fetchFromTMBD('tv/popular');
+    
+    results.forEach(show => {
+        const src = show.poster_path ? `https://image.tmdb.org/t/p/w500${show.poster_path}` : 'images/no-image.jpg';
+        const parent = document.querySelector('#popular-shows');
+        const child = document.createElement('div');
+        child.classList.add('card');
+        const cardBody = showCard(show.name, show.first_air_date, src);
         child.innerHTML += cardBody;
         parent.appendChild(child);
 
@@ -72,11 +106,10 @@ const init = () => {
     switch (global.currentPage) {
         case '/':
         case '/index.html':
-            fetchFromTMBD('movie/popular');
             renderMovies();
             break;
         case '/shows.html':
-            console.log('Shows Page');
+            renderShows();
             break;
         case '/movie-details.html':
             console.log('Movie Details Page');
