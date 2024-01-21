ec85c432f764947bafb11eeb2454a4c95d1df1ae
Alvaro Castro Cid <alvarocastrocid@Alvaros-MacBook-Pro.local>
2024-01-21 08:27:54 -0500

    commented out config file;

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import config from "./config.js";
+// import config from "./config.js";
 import render from "./utilities/render.js";
 import swiperSlide from "./components/SwiperSlide.js";
 import innerResultsHeading from "./components/InnerResultsHeading.js";
